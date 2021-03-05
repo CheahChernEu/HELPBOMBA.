80b243b66ce00e4161f5f1e5813200307594046b
ERIC <78393466+Eric1479@users.noreply.github.com>
2021-03-05 16:28:13 +0800

    Added footer

--- manageVolunteerProfile.php
+++ manageVolunteerProfile.php
@@ -19,16 +19,16 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                   <ul class="navbar-nav ml-auto float-right text-right">
                     <li class="nav-item">
-                        <a class="nav-link ml-5" href="managevolunteerprofile.php">Manage Profile</a>
+                        <a class="nav-link ml-5" href="managevolunteerprofile.html">Manage Profile</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link ml-5" href="applyfortrip.html">Apply For Trip</a>
+                        <a class="nav-link ml-5" href="applyForTrip.html">Apply For Trip</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link ml-5" href="viewapplication.html">View Application</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link ml-5" href="homepage.php">Log Out</a>
+                        <a class="nav-link ml-5" href="homepage.html">Log Out</a>
                     </li>
                   </ul>
                 </div>
@@ -36,7 +36,9 @@
               </nav>
         </header>
 
+        <section id="form">
         <div class="form col-6 col-md-6 col-sm-6">
+            <div class="row">
             <h2>Manage Profile</h2>
             <p>*Required fields</p>
             <form name="volunteerform" action="managevolunteerprofile.html" enctype="multipart/form-data">
@@ -90,7 +92,44 @@
                     <button type="button" value="Submit" onclick="blankOldPwValidation(), blankNewPwValidation(), pwValidation(), nameValidation(), phoneValidation(), docTypeBlankValidation(), checkDate(), submitMessage()">Submit</button>
                 </div>
             </form>
+            </div>
         </div>
+        </section>
+
+        <section id="contact">
+        <footer class="py-5">
+            <div class="container" py-5>
+                <div class="row">
+                    <div class="col-md-5 col-sm-6">
+                        <h2>CRS Sdn. Bhd.</h2>
+                        <p>Wisma Help, Jalan Dungun, Bukit Damansara,<br>50490 Kuala Lumpur,<br>Wilayah Persekutuan Kuala Lumpur</p>
+                    </div>
+
+                    <div class="col-md-4 col-sm-6">
+                        <div class="footer-info">
+                            <h2>Keep In Touch</h2>
+                            <p><a href="#">016-1234567</a></p>
+                            <p><a href="#">crs@gmail.com</a></p>
+                            <p><a href="#">Our Location</a></p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 col-sm-12">
+                        <div class="footer-info">
+                            <h2>About Us</h2>
+                            <p>Crisis Relief Services (CRS) is an NGO (Non-Government Organization) that aims to help people who are facing crises arising from natural disasters such as flood and earthquakes.</p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-12 col-12 text-center">
+                        <div class="copyright-text">
+                            <p>Copyright @ 2021 <a href="#">CRS Organization</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        </section>
 
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
