af33bb2d3ed4cfb09a1333a1a7b872f23a6a8ade
CHEAH CHERN EU <tennischeah@gmail.com>
2021-03-07 18:45:46 +0800

    sign up updates

--- signUp.php
+++ signUp.php
@@ -3,15 +3,14 @@
 $conn = mysqli_connect("localhost","root","","helpbomba");
 
 $username = $_POST['signUpUsername'];
-$password =$_POST['signUpPw'];
-$position =$_POST['roleOptions'];
+$password = $_POST['signUpPw'];
+$position = $_POST['roleOptions'];
 
 $sql = "INSERT INTO `hbmember`( `username`, `password`, `position` ) VALUES ('$username','$password','$position')";
 $insert = mysqli_query($conn,$sql);
 if(!$insert){
   echo"ERROR";
 }else{
-  header("Location:homepage.php?error=invalidSignUp");
-  echo <script> window.location.assign('homepage.php');</script>;
+  echo "<script> window.location.assign('homepage.php');</script>";
 }
  ?>
