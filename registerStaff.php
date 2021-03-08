75f0e991b66f1d73a31ff2972119f54841cb1097
CHEAH CHERN EU <tennischeah@gmail.com>
2021-03-08 10:33:11 +0800

    registerStaff done

--- registerStaff.php
+++ registerStaff.php
@@ -2,6 +2,26 @@
 
 $conn = mysqli_connect("localhost","root","","helpbomba");
 
+function db_search($sql){
+
+  $servername = "localhost";
+  $username   = "root";
+  $password   = "";
+  $dbname     = "helpbomba";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  $result = $conn->query($sql);
+
+  return $result->fetch_object();
+}
+
 $username = $_POST['usernameStaff'];
 $password = $_POST['passwordStaff'];
 $name = $_POST['nameStaff'];
@@ -9,6 +29,13 @@ $phoneNo = $_POST['phoneNoStaff'];
 $position = $_POST['positionStaff'];
 $dateJoined = $_POST['dateJoinedStaff'];
 
+
+  $sql1="SELECT * FROM `hbmember` WHERE username = '$username'";
+  $select = db_search($sql1);
+if($select!=null){
+  echo '<script> alert("This staff has been existed")</script>';
+  echo "<script> window.location.assign('login.php');</script>";
+}else{
 
    $sql2 = "INSERT INTO `hbmember`(`username`, `password`, `name`, `contactNo`, `position`, `dateJoined` ) VALUES ('$username', '$password', '$name', '$phoneNo', '$position', '$dateJoined' )";
 
@@ -19,7 +46,7 @@ $dateJoined = $_POST['dateJoinedStaff'];
      echo '<script> alert("This staff has been added successfully")</script>';
      echo "<script> window.location.assign('manager.php');</script>";
    }
-
+}
 
 
 
