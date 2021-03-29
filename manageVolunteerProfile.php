2a877c0a798510a72f0f1f7e33a403f2392a424b
ERIC <78393466+Eric1479@users.noreply.github.com>
2021-03-29 13:49:25 +0800

    Updated css

--- manageVolunteerProfile.php
+++ manageVolunteerProfile.php
@@ -12,7 +12,7 @@ Student ID: B1801134
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
-        <link rel="stylesheet" type="text/css" href="managevolunteerprofile.css">
+        <link rel="stylesheet" type="text/css" href="manageVolunteerProfile.css">
     </head>
     <body>
         <header>
